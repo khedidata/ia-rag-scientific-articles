ac1de288bb8e27ea0cf2fd097a807d279de4171a
MourZ <zemourkhedi@yahoo.fr>
2025-08-30 02:31:21 +0200

    Scraping and Data Preprocessor Files

--- data_collection/__init__.py
+++ data_collection/__init__.py
@@ -1,4 +1,4 @@
 from .collector import *
 
 __all__ = ["fetch_arxiv_feed", "fetch_all_category_entries",
-                      "fetch_all_cs_entries", "main_articles_collection"] 
+           "fetch_all_cs_entries", "main_articles_collection"] 
